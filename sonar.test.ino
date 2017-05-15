f1ac3b3785f68b8e3e41ae836b1bd3821c22b096
Waqas <ssuet_waqas@hotmail.com>
2017-05-15 14:17:22 +0500

    Add files via upload

---
+++ sonar.test.ino
@@ -0,0 +1,17 @@
+#include <NewPing.h>
+#define MAX_DISTANCE 10 
+#define TRIGGER_PIN 9
+#define ECHO_PIN 10
+
+NewPing sonar(TRIGGER_PIN,ECHO_PIN,MAX_DISTANCE);
+
+void setup() 
+{
+  Serial.begin(9200);
+}
+void loop() 
+{
+delay(50);
+unsigned int PING = sonar.ping_cm();
+Serial.println ("DISTANCE : " + String(PING) + "cm"); 
+}
